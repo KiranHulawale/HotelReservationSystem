fb82092ce641fda07677d8bfde41b6b5eca22b77
Kiran <kiranhulawale123@gmail.com>
2022-04-16 20:58:18 +0530

    Welcome to Hotel Reservation System

---
+++ src/com/bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation system");
+    }
+    }
